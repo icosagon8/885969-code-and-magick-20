544d7dab85b68059d70be0aae3dcd760699e2546
icosagon8 <dmpavlov8@yandex.ru>
2020-06-01 21:48:38 +0300

    Исправил замечания наставника

--- js/stat.js
+++ js/stat.js
@@ -8,6 +8,7 @@ var SHADOW_X = 10;
 var SHADOW_Y = 10;
 var GAP = 20;
 var FONT_GAP = 20;
+var BARS_LEFT_GAP = 20;
 var BAR_DELTA = 50;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
@@ -26,34 +27,41 @@ var getMaxElement = function (arr) {
       maxElement = arr[i];
     }
   }
-
   return maxElement;
+};
+
+var splitString = function (ctx, line) {
+  var lines = line.split('\n');
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  for (var i = 0; i < lines.length; i++) {
+    ctx.fillText(lines[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP * i);
+  }
+};
+
+var drawingBar = function (ctx, names, periods) {
+  var maxTime = getMaxElement(periods);
+
+  for (var i = 0; i < names.length; i++) {
+    ctx.textBaseline = 'bottom';
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(names[i], CLOUD_X + GAP + BARS_LEFT_GAP + (BAR_WIDTH + BAR_DELTA) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(240, ' + Math.round(Math.random() * 100) + '%, 50%)';
+    }
+    ctx.fillRect(CLOUD_X + GAP + BARS_LEFT_GAP + (BAR_WIDTH + BAR_DELTA) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - (BAR_HEIGHT * periods[i]) / maxTime - FONT_GAP, BAR_WIDTH, (BAR_HEIGHT * periods[i]) / maxTime);
+    ctx.textBaseline = 'top';
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(Math.round(periods[i]), CLOUD_X + GAP + BARS_LEFT_GAP + (BAR_WIDTH + BAR_DELTA) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP - (BAR_HEIGHT * periods[i]) / maxTime - FONT_GAP);
+  }
 };
 
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + SHADOW_X, CLOUD_Y + SHADOW_Y, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-
-  ctx.fillStyle = TEXT_COLOR;
-
-  var maxTime = getMaxElement(times);
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
-
-  for (var i = 0; i < players.length; i++) {
-    ctx.textBaseline = 'bottom';
-    ctx.fillStyle = TEXT_COLOR;
-    ctx.fillText(players[i], CLOUD_X + 2 * GAP + (BAR_WIDTH + BAR_DELTA) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsl(240, ' + Math.round(Math.random() * 100) + '%, 50%)';
-    }
-    ctx.fillRect(CLOUD_X + 2 * GAP + (BAR_WIDTH + BAR_DELTA) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - (BAR_HEIGHT * times[i]) / maxTime - FONT_GAP, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
-    ctx.textBaseline = 'hanging';
-    ctx.fillStyle = TEXT_COLOR;
-    ctx.fillText(Math.round(times[i]), CLOUD_X + 2 * GAP + (BAR_WIDTH + BAR_DELTA) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - (BAR_HEIGHT * times[i]) / maxTime - 2 * FONT_GAP);
-  }
+  splitString(ctx, 'Ура вы победили!\nСписок результатов:');
+  drawingBar(ctx, players, times);
 };
